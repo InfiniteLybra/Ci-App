5e45e9275f7cfb3e9b13111b4843c168f6f1b487
InfiniteLybra <mfawwaz1807@gmail.com>
2023-09-05 22:15:33 +0700

    first commit

---
+++ application/models/Mahasiswa_model.php
@@ -0,0 +1,31 @@
+<?php
+
+class Mahasiswa_model extends CI_Model {
+    public function getAllMahasiswa()
+    {
+        return $this->db->get('mahasiswa')->result_array();
+    }
+
+    public function TambahDataMahasiswa()
+    {
+        $data = [
+            "nama" => $this->input->post('nama', true),
+            "nrp" => $this->input->post('nrp', true),
+            "email" => $this->input->post('email', true),
+            "jurusan" => $this->input->post('jurusan', true),
+        ];
+
+        $this->db->insert('mahasiswa', $data);
+    }
+
+    public function HapusDataMahasiswa($id)
+    {
+        $this->db->where('id', $id);
+        $this->db->delete('mahasiswa');
+    }
+
+    public function getMahasiswaById($id)
+    {
+        return $this->db->get_where('mahasiswa', ['id' => $id])->row_array();
+    }
+}
